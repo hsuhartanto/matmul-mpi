89c2d0041bb3d4db27c9857c37afb5d0a130e49b
hsuhartanto <herusuhartanto@gmail.com>
2026-04-12 19:51:20 +0700

    Update run_matmul.sh

--- run_matmul.sh
+++ run_matmul.sh
@@ -4,12 +4,11 @@
 #SBATCH -N 2
 #SBATCH --nodelist=node-06,node-08
 
-PROG=/home/user01/matmul
 INFO="my name and my IDs number"
 
 
-mpirun --mca btl_tcp_if_exclude docker0,lo -np 8 $PROG 512 "$INFO"
-mpirun --mca btl_tcp_if_exclude docker0,lo -np 16 $PROG 512 "$INFO"
-mpirun --mca btl_tcp_if_exclude docker0,lo -np 8 $PROG 1024 "$INFO"
-mpirun --mca btl_tcp_if_exclude docker0,lo -np 16 $PROG 1024 "$INFO"
+mpirun --mca btl_tcp_if_exclude docker0,lo -np 8 ./matmul  512 "$INFO"
+mpirun --mca btl_tcp_if_exclude docker0,lo -np 16 ./matmul  512 "$INFO"
+mpirun --mca btl_tcp_if_exclude docker0,lo -np 8 ./matmul  1024 "$INFO"
+mpirun --mca btl_tcp_if_exclude docker0,lo -np 16 ./matmul  1024 "$INFO"
 
